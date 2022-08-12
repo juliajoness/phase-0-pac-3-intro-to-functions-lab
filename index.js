f57fb1807b0efe71bd2f188acd1209e96152f280
julia <juliajaj3@gmail.com>
2022-08-12 13:58:12 -0400

    commit tree 3d573533c709d5a8acc0b7221836d178f57c8292

---
+++ index.js
@@ -0,0 +1,41 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    // todo
+  }
+  function whisper(string) {
+    return string;
+  }
+  "Hello!".toLowerCase(); // 'HELLO!'
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logShout (aWord) {
+    console.log(aWord.toUpperCase( ));
+  }
+  function logWhisper (aWord) {
+    console.log(aWord.toLowerCase( ));
+  }
+  function sayHiToHeadphonedRoommate (aWord) {
+    var cantUnswer = "I can't hear you!";
+    var yesUnswer = "YES INDEED!";
+    var lovUnswer = "I would love to!";
+    if (aWord.toLowerCase(aWord) === aWord) {
+      return cantUnswer;
+    }
+    else if (aWord.toUpperCase(aWord) === aWord) {
+      return yesUnswer;
+    }
+    else if ( "Let's have dinner together!" === aWord) {
+      return lovUnswer
+    }
+  }
+  
